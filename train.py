d28669d5a7c2793daf3784ac0e4156dc31b0e3c3
Hiep Duong <duongduchiepdtv51@gmail.com>
2019-12-05 13:34:10 +0700

    add test loss

--- train.py
+++ train.py
@@ -36,13 +36,17 @@ def main():
     training_set = TitleDataset('data/train.csv',
                                 tokenizer=bert_tokenizer,
                                 max_full_text_len=512)
+    test_set = TitleDataset('data/test.csv',
+                            tokenizer=bert_tokenizer,
+                            max_full_text_len=512)
 
     training_generator = data.DataLoader(training_set, **params)
+    test_generator = data.DataLoader(test_set, **params)
 
-    return (training_generator, model)
+    return (training_generator, test_generator, model)
 
 
-def train(training_generator, model, device='cpu', max_epochs=2):
+def train(training_generator, test_generator, model, device='cpu', max_epochs=2):
     model.to(device)
     optimizer = optim.Adam(model.parameters(), **opim_params)
     for epoch in range(max_epochs):
@@ -68,9 +72,16 @@ def train(training_generator, model, device='cpu', max_epochs=2):
                 print('Epoch: {}, step: {}, loss: {}'.format(epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
 
+    running_loss = 0.0
+    with torch.set_grad_enabled(False):
+        for input_ids, label in test_generator:
+            outputs = model(input_ids, labels=label)
 
+            loss = outputs[0].squeeze()
+            running_loss += loss.item()
+    print("Validation loss: {}".format(running_loss))
 
 if __name__ == "__main__":
-    training_generator, model = main()
+    training_generator, test_generator, model = main()
     # input_ids, labels_1, labels_2 = next(iter(training_generator))
     train(training_generator=training_generator, model=model, device=device, max_epochs=10)
